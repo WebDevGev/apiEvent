524ac7f65b4fa79dcd8c8b02e50ede6a640804da
Gev <g0808m@gmail.com>
2021-09-15 10:47:18 +0300

    creating swagger documentation

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { DataTypes } from "sequelize";
 import { Column, DataType, Table, Model} from "sequelize-typescript";
 
@@ -9,15 +10,24 @@ interface UserCreationAttrs {
 
 @Table({tableName:'users'})
 export class User extends Model<User, UserCreationAttrs > {
+    @ApiProperty({example: '1', description: 'uniqe id'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
     id: number;
+
+    @ApiProperty({example: 'user@gmail.com', description: 'Email adress'})
     @Column({ type: DataType.STRING, unique: true, allowNull: false})
     email: string;
+
+    @ApiProperty({example: '12345678', description: 'Password'})
     @Column({ type: DataType.STRING, allowNull: false })
     password: string;
+
+    @ApiProperty({example: 'Gev', description: "User's name"})
     @Column({ type: DataType.STRING })
     name: string;
+
+    @ApiProperty({example: 'true', description: 'organizator'})
     @Column({type: DataTypes.BOOLEAN, defaultValue: false})
-    admin: boolean
+    organizator: boolean
 
 }
